4c4864ce2d730d9b649c63d7573860709255afa5
veginativenkatasuresh <sureshveginati000@gmail.com>
2023-03-09 12:25:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import Course from './Layout/Navigation/Useeffect';
+// import Name from './Layout/Navigation/Useref';
+import Name1 from './Components/Course/Memo';
+function App() {
+  return (
+     <div className="App">
+      
+      <Course/>
+      <Name/>
+     </div>
+  );
+}
+export default App;
